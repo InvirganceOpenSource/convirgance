ff94f1f4af77e55c32277572a9cb25071d98759a
ethanivg <ethan.henry@invirgance.com>
2024-12-26 20:25:56 -0600

    docs-JSONObject: JSONObject constructor reword

--- src/main/java/com/invirgance/convirgance/json/JSONObject.java
+++ src/main/java/com/invirgance/convirgance/json/JSONObject.java
@@ -82,7 +82,8 @@ public class JSONObject implements Map<String, Object>
     
     /**
       * Creates a JSONObject based on the provided map parameter. 
-      * If the map is a JSONObject and is ordered, a shallow copy of the ordered keys is made.
+      * If the map is an instance of JSONObject and is ordered, 
+      * this JSONObject will inherit the ordering and its ordered keys.
       * 
       * @param map A map of key-value pairs.
      */
