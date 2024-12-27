d7c0bfc455cfc7bd5b946a5cfffe682602b06b57
ethanivg <ethan.henry@invirgance.com>
2024-12-26 18:40:20 -0600

    docs-JSONObject: constructor nits

--- src/main/java/com/invirgance/convirgance/json/JSONObject.java
+++ src/main/java/com/invirgance/convirgance/json/JSONObject.java
@@ -81,10 +81,12 @@ public class JSONObject implements Map<String, Object>
     }
     
     /**
-     * Creates a JSONObject based on the input map. 
-     * Sets the objects ordered state to true and populates a new OrderedKeys collection 
-     * with the current passed in objects keys. Additionally sets the objects map to the passed in value.
-     * @param map A map of key value pairs.
+      * Creates a JSONObject based on the provided map parameter. 
+      * If the map is a JSONObject and is ordered, a shallow copy of the ordered keys is made.
+      * Additionally, this object's internal map is populated with the contents of the provided map.
+      * 
+      * @param map A map of key-value pairs. If the map is an instance of JSONObject and is ordered, 
+      *            this JSONObject will inherit the ordering and its ordered keys.
      */
     public JSONObject(Map<String, Object> map)
     {
