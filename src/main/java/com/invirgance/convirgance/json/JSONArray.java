fc2a160476e751cbadca321409635dac376afcba
ethanivg <ethan.henry@invirgance.com>
2024-12-27 20:57:44 -0600

    docs-JSONArray: getJSONArray:default docs

--- src/main/java/com/invirgance/convirgance/json/JSONArray.java
+++ src/main/java/com/invirgance/convirgance/json/JSONArray.java
@@ -253,10 +253,15 @@ public class JSONArray<T> implements List<T>
     }
 
     /**
+     * Gets the JSONArray at the given index. Returns the provided default if
+     * the index's value is null.
      *
-     * @param index
-     * @param defaultValue
-     * @return
+     * @param index The index.
+     * @param defaultValue A JSONArray to return when index's value is null.
+     * @return The provided default when index's value is null, otherwise
+     * returns the index's value.
+     * @throws ConvirganceException If the value exists but is not a JSONArray.
+     *
      */
     public JSONArray getJSONArray(int index, JSONArray defaultValue)
     {
