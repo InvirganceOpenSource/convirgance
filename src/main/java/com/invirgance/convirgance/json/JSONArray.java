82531c2ded5027a346460557ad225786dbde63c4
ethanivg <ethan.henry@invirgance.com>
2024-12-27 21:34:09 -0600

    docs-JSONArray: getJSONObject nit

--- src/main/java/com/invirgance/convirgance/json/JSONArray.java
+++ src/main/java/com/invirgance/convirgance/json/JSONArray.java
@@ -245,7 +245,7 @@ public class JSONArray<T> implements List<T>
     /**
      * Gets the JSONObject at the given index.
      *
-     * @param index The index.
+     * @param index Position in this array to retrieve the value from.
      * @return The JSONObject at the given index, or null if the value is null.
      * @throw ConvirganceException When the index's value exists but is not a
      * JSONObject.
