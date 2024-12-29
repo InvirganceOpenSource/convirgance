5572d2f5d3fb80ffa75e14d60938cb9851a278fe
ethanivg <ethan.henry@invirgance.com>
2024-12-28 22:21:17 -0600

    docs-JSON: package-info

---
+++ src/main/java/com/invirgance/convirgance/json/package-info.java
@@ -0,0 +1,39 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2024 tadghh.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+* Provides classes for reading, writing, and manipulating JSON data.
+* <p>
+* Key classes include:
+* <ul>
+*   <li>{@link JSONArray} - A List implementation for working with JSON arrays</li>
+*   <li>{@link JSONObject} - A Map implementation for working with JSON objects</li>
+*   <li>{@link JSONParser} - Parses JSON text into objects and arrays</li>
+*   <li>{@link JSONWriter} - Writes JSON objects and arrays to an output stream</li>
+* </ul>
+* <p>
+* This package supports standard JSON data types and formatting according to the
+* JSON specification.
+*/
+package com.invirgance.convirgance.json;
