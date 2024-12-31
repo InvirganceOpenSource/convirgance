7fedeaa188d63cac76ad8fa4abf2c4203c5bf2e5
ethanivg <ethan.henry@invirgance.com>
2024-12-30 21:23:21 -0600

    docs-InputCursor: interface doc

--- src/main/java/com/invirgance/convirgance/input/InputCursor.java
+++ src/main/java/com/invirgance/convirgance/input/InputCursor.java
@@ -24,7 +24,9 @@ package com.invirgance.convirgance.input;
 import com.invirgance.convirgance.CloseableIterator;
 
 /**
- *
+ * A cursor interface for iterating over data read by an Input implementation.
+ * Extends Iterable to provide type-safe iteration while ensuring resources are
+ * properly closed via CloseableIterator.
  * @author jbanes
  */
 public interface InputCursor<T> extends Iterable<T>
