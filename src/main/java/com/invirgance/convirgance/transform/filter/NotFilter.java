49b81e77390e332ad9bc96370f7bac90eda22006
ethanivg <ethan.henry@invirgance.com>
2025-01-03 04:31:33 -0600

    docs-NotFilter: docs

--- src/main/java/com/invirgance/convirgance/transform/filter/NotFilter.java
+++ src/main/java/com/invirgance/convirgance/transform/filter/NotFilter.java
@@ -24,32 +24,52 @@ package com.invirgance.convirgance.transform.filter;
 import com.invirgance.convirgance.json.JSONObject;
 
 /**
- *
+ * Filters JSONObjects that do not meet the condition of an existing filter.
  * @author jbanes
  */
 public class NotFilter implements Filter
 {
     private Filter filter;
 
+    /**
+     * Creates a NotFilter without an initial filter.
+     */
     public NotFilter()
     {
     }
 
+    /**
+     * Creates a new NotFilter with the provided filter to invert.
+     * @param filter The filter to invert.
+     */
     public NotFilter(Filter filter)
     {
         this.filter = filter;
     }
 
+    /**
+     * Gets the current filter that will be inverted during evaluation.
+     * @return The current filter.
+     */
     public Filter getFilter()
     {
         return filter;
     }
 
+    /**
+     * Set the filter to invert during comparison.
+     * @param filter The filter.
+     */
     public void setFilter(Filter filter)
     {
         this.filter = filter;
     }
     
+    /**
+     * Evaluates the record against the filter and returns the opposite result.
+     * @param record The record to evaluate.
+     * @return The negated result of the filter evaluation.
+     */
     @Override
     public boolean test(JSONObject record)
     {
