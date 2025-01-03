28b1c5ffbca524df341af7bf764625e730312cd7
ethanivg <ethan.henry@invirgance.com>
2025-01-03 04:21:01 -0600

    docs-LessThanOrEqualFilter: docs

--- src/main/java/com/invirgance/convirgance/transform/filter/LessThanOrEqualFilter.java
+++ src/main/java/com/invirgance/convirgance/transform/filter/LessThanOrEqualFilter.java
@@ -24,21 +24,36 @@ package com.invirgance.convirgance.transform.filter;
 import com.invirgance.convirgance.json.JSONObject;
 
 /**
- *
+ * Filters JSONObjects where the value of the specified key is less than or equal to the provided comparison value.
  * @author jbanes
  */
 public class LessThanOrEqualFilter extends ComparatorFilter
 {
+    /**
+     * Creates a new LessThanOrEqualFilter.
+     */
     public LessThanOrEqualFilter()
     {
         super();
     }
 
+    /**
+     * Creates a new LessThanOrEqualFilter for evaluating whether the value of a
+     * specified key is less than or equal to the comparison value.
+     *
+     * @param key The key to evaluate.
+     * @param value The comparison value.
+     */
     public LessThanOrEqualFilter(String key, Object value)
     {
         super(key, value);
     }
     
+    /**
+     * Tests if the value of the specified key in the record is less than or equal to the comparison value.
+     * @param record The record to evaluate.
+     * @return True if the value of the record's specified key is less than or equal to the comparison value.
+     */    
     @Override
     public boolean test(JSONObject record)
     {
