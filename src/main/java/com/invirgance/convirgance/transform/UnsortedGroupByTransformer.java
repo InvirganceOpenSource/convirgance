a7595da696c7d07e06b916cb16aec220f71b478f
ethanivg <ethan.henry@invirgance.com>
2025-01-08 16:13:06 -0600

    added: UnsortedGroupBy

---
+++ src/main/java/com/invirgance/convirgance/transform/UnsortedGroupByTransformer.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2024 INVIRGANCE LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy 
+of this software and associated documentation files (the “Software”), to deal 
+in the Software without restriction, including without limitation the rights to 
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+of the Software, and to permit persons to whom the Software is furnished to do 
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE.
+ */
+package com.invirgance.convirgance.transform;
+
+import com.invirgance.convirgance.json.JSONArray;
+import com.invirgance.convirgance.json.JSONObject;
+import java.util.*;
+
+/**
+ * Groups data together from an iterator based on a specified keys.
+ * 
+ * @author tadghh
+ */
+public class UnsortedGroupByTransformer implements Transformer 
+{   
+    private final String[] groupByKeys; 
+    private final String outputKey;   
+
+    /**
+     * Creates a new UnsortedGroupByTransformer to group related data on provided fields. 
+     * @param keys The fields we want to group related data on.
+     * @param output The field to output the grouped data to.
+     */
+    public UnsortedGroupByTransformer(String[] keys, String output)
+    {
+        this.groupByKeys = keys;
+        this.outputKey = output;
+    }
+     
+    /**
+     * Groups unsorted JSONObjects based on a specific matching key value.
+     * Ex Collecting atomized weather data for cities and grouping it together.
+     * 
+     * @param sourceIterator The iterator of JSONObjects.
+     * @return A new iterator with the grouped data.
+     */
+    @Override
+    public Iterator<JSONObject> transform(Iterator<JSONObject> sourceIterator)
+    {
+        String header;
+        JSONArray groupArray;
+        JSONObject record;
+        JSONObject collected; 
+        
+        Map<String, JSONArray> groupArrays = new HashMap();
+        List<JSONObject> groups = new ArrayList<>();
+        Set<String> groupKeySet = new HashSet<>(Arrays.asList(groupByKeys));     
+        
+        while (sourceIterator.hasNext())
+        {
+            record = sourceIterator.next();
+            header = createGroupKey(record);
+            groupArray = groupArrays.get(header);
+            
+            // Create new group only when needed
+            if (groupArray == null)
+            {
+                collected = new JSONObject();
+                groupArray = new JSONArray();
+
+                // Set group keys
+                for (String key : groupByKeys)
+                {
+                    collected.put(key, record.get(key));
+                }
+
+                collected.put(outputKey, groupArray);
+                groupArrays.put(header, groupArray);
+                groups.add(collected);
+            }
+
+            record.keySet().removeAll(groupKeySet);
+            groupArray.add(record);
+        }
+
+        return groups.iterator();
+    }
+
+    private String createGroupKey(JSONObject record)
+    {
+        Object[] values = new Object[groupByKeys.length];
+        
+        for (int i = 0; i < groupByKeys.length; i++)
+        {
+            values[i] = record.get(groupByKeys[i]);
+        }
+        
+        return Arrays.toString(values);
+    }
+    
+}
