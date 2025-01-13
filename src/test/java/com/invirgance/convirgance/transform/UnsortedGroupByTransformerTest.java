ab51c72dd6c17aaa213ee35d93e390f7890b1e9c
ethanivg <ethan.henry@invirgance.com>
2025-01-13 14:09:56 -0600

    tests: UnsortedGroupByTransformer

--- src/test/java/com/invirgance/convirgance/transform/UnsortedGroupByTransformerTest.java
+++ src/test/java/com/invirgance/convirgance/transform/UnsortedGroupByTransformerTest.java
@@ -23,8 +23,10 @@
  */
 package com.invirgance.convirgance.transform;
 
+import com.invirgance.convirgance.ConvirganceException;
 import com.invirgance.convirgance.json.JSONArray;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -74,13 +76,155 @@ public class UnsortedGroupByTransformerTest
         JSONArray objects = new JSONArray(testItems);
         JSONArray expected = new JSONArray(equalTest);
         
-        transformer.transform(objects).forEach(elem -> transformed.add(elem));
-        
-        System.out.println(transformed.toString());
-        System.out.println(expected.toString());
-        System.out.println("\nTemp Array Elements and HashCodes:");
-        System.out.println("\nTemp Array Temps HashCodes:");
+        transformer.transform(objects).forEach(elem -> transformed.add(elem));       
 
         assertEquals(transformed.toString(),expected.toString());
     }    
+    
+    /**
+     * Make sure nested values are grouped correctly
+     */
+    @Test
+    public void testTransformNested()
+    {
+        String equal = "[{\"city\":\"Tampa\",\"temps\":[{\"details\":{\"temp\":35.2,\"weather\":\"rain\"}},{\"details\":{\"temp\":36.2,\"weather\":\"sunny\"}},{\"details\":{\"temp\":32.1,\"weather\":\"overcast\"}},{\"details\":{\"temp\":22.1,\"weather\":\"overcast\"}}]},{\"city\":\"Milwaukee\",\"temps\":[{\"details\":{\"temp\":23.2,\"weather\":\"sunny\"}}]}]";
+        String nested = "[{\"city\": \"Tampa\", \"details\": {\"temp\": 35.2, \"weather\": \"rain\"}},"
+          + "{\"city\": \"Milwaukee\", \"details\": {\"temp\": 23.2, \"weather\": \"sunny\"}},"
+          + "{\"city\": \"Tampa\", \"details\": {\"temp\": 36.2, \"weather\": \"sunny\"}},"
+          + "{\"city\": \"Tampa\", \"details\": {\"temp\": 32.1, \"weather\": \"overcast\"}},"
+          + "{\"city\": \"Tampa\", \"details\": {\"temp\": 22.1, \"weather\": \"overcast\"}}]";
+        
+        JSONArray objects = new JSONArray(nested);
+        JSONArray expected = new JSONArray(equal);
+         
+        transformer.transform(objects).forEach(elem -> transformed.add(elem));
+
+        assertEquals(transformed.toString(),expected.toString());
+    }    
+
+    /**
+     * Test that grouping on multiple fields works as expected.
+     */
+    @Test
+    public void testTransformMultipleFields()
+    {
+        String equalTest = "[{\"city\":\"Tampa\",\"weather\":\"rain\",\"temps\":[{\"temp\":35}]},{\"city\":\"Milwaukee\",\"weather\":\"sunny\",\"temps\":[{\"temp\":23}]},{\"city\":\"Tampa\",\"weather\":\"sunny\",\"temps\":[{\"temp\":36}]},{\"city\":\"Tampa\",\"weather\":\"overcast\",\"temps\":[{\"temp\":32},{\"temp\":22}]}]";
+        String[] keys = new String[]
+        {
+            "city","weather"
+        };
+        
+        JSONArray objects = new JSONArray(testItems);
+        JSONArray expected = new JSONArray(equalTest);
+        
+        UnsortedGroupByTransformer transformer = new UnsortedGroupByTransformer(keys, "temps");
+
+        transformer.transform(objects).forEach(elem -> transformed.add(elem));
+        
+        assertEquals(transformed.toString(),expected.toString());
+    }
+
+    /**
+     * Test that grouping on a missing field works (although a null value is added).
+     */
+    @Test
+    public void testTransformOneMissingField()
+    {
+        String equalTest = "[{\"keyboard\":null,\"city\":\"Tampa\",\"temps\":[{\"temp\":35,\"weather\":\"rain\"},{\"temp\":36,\"weather\":\"sunny\"},{\"temp\":32,\"weather\":\"overcast\"},{\"temp\":22,\"weather\":\"overcast\"}]},{\"keyboard\":null,\"city\":\"Milwaukee\",\"temps\":[{\"temp\":23,\"weather\":\"sunny\"}]}]";
+      
+        String[] keys = new String[]
+        {
+            "city","keyboard"
+        };
+             
+        JSONArray objects = new JSONArray(testItems);
+        JSONArray expected = new JSONArray(equalTest);
+        
+        UnsortedGroupByTransformer transformer = new UnsortedGroupByTransformer(keys, "temps");
+        transformer.transform(objects).forEach(elem -> transformed.add(elem));
+
+        assertEquals(transformed.toString(),expected.toString());
+    }
+
+    /**
+     * Tests that grouping on fields with values that contain the output key
+     * preserves the data correctly (nested).
+     */
+//    Bad Test
+//    @Test
+//    public void testTransformDuplicateFields()
+//    {       
+//        String testItems = "[{\"city\": \"Tampa\", \"temp\": 35.2}, {\"city\": \"Tampa\", \"temps\": [36.2]}]";
+//        String expectedDuplicate = "[{\"city\":\"Tampa\",\"temps\":[{\"temp\":35.2},{\"temps\":[36.2]}]}]";
+//        
+//        JSONArray objects = new JSONArray(testItems);
+//        JSONArray expected = new JSONArray(expectedDuplicate);
+//        
+//        String[] groupKeys = new String[]
+//        {
+//            "city"
+//        };
+//        
+//        UnsortedGroupByTransformer transformer = new UnsortedGroupByTransformer(groupKeys, "temps");
+//
+//        transformer.transform(objects).forEach(transformed::add);
+//
+//        // Verify transformation
+//        assertTrue(expected.equals(transformed));
+//    }
+//
+//    Bad Test     
+//    /**
+//     * Fields should be case sensitive.
+//     */
+//    @Test
+//    public void testCaseSensitivityInKeys()
+//    {
+//        String testItems = "[{\"city\": \"Tampa\", \"temp\": 35.2}, {\"City\": \"Tampa\", \"temp\": 36.2}]";
+//        String equalObj = "[{\"city\":\"Tampa\",\"temps\":[{\"temp\":35.2}]},{\"city\":null,\"temps\":[{\"City\":\"Tampa\",\"temp\":36.2}]}]";        
+//        
+//        JSONArray objects = new JSONArray(testItems);
+//        JSONArray objectsEq = new JSONArray(equalObj);
+//     
+//        transformer.transform(objects).forEach(transformed::add);
+//
+//        assertTrue(objectsEq.equals(transformed));
+//    }
+    
+    /**
+     * Test that grouping on empty fields will throw.
+     */
+    @Test
+    public void testTransformSillyFields()
+    {       
+        String[] keys = new String[] {"Fish", ""};       
+
+        // Verify that creating the transformer with empty keys throws an exception
+        Exception exception = assertThrows(ConvirganceException.class, () ->
+        {
+            new UnsortedGroupByTransformer(keys, "temps");
+        });
+
+        // Assert that the exception message matches what is expected
+        assertEquals("Fields must not contain null or empty values.", exception.getMessage());
+    }
+
+    /**
+     * Test that an exception will throw when no output key is provided.
+     */
+    @Test
+    public void testTransformNoOutput()
+    {       
+        String[] keys = new String[] {"Fish", "Dog"};       
+
+        // Verify that creating the transformer with no output throws an exception
+        Exception exception = assertThrows(ConvirganceException.class, () ->
+        {
+            new UnsortedGroupByTransformer(keys, "");
+        });
+
+        // Assert that the exception message matches what is expected
+        assertEquals("Output key must not be null or empty.", exception.getMessage());
+    }
+ 
 }
