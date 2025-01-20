a72627cc0e84b30e4ad87dbd7a146c5330e9c190
ethanivg <ethan.henry@invirgance.com>
2025-01-20 13:51:28 -0600

    rename: BSON -> JBIN tests

--- src/test/java/com/invirgance/convirgance/jbin/BinaryDecoderTest.java
+++ src/test/java/com/invirgance/convirgance/jbin/BinaryDecoderTest.java
@@ -19,7 +19,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
 SOFTWARE.
  */
-package com.invirgance.convirgance.bson;
+package com.invirgance.convirgance.jbin;
 
 import com.invirgance.convirgance.jbin.BinaryDecoder;
 import com.invirgance.convirgance.jbin.KeyTableEncoder;
